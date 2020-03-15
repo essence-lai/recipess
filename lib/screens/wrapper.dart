76fb7202b917cc22a423ab4e4084a78b4d172e0e
Essence <essence.lai@alumni.ubc.ca>
2020-03-15 16:54:55 -0700

    basic app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:recipess/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    // return authenticate or home
+    return Home();
+  }
+}
